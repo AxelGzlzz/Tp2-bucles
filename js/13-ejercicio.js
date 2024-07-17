38c65b3b7d7c5ff2bf07f39dd4f811e3322d1138
axel gonzalez <ag180066@gmail.com>
2024-07-17 15:32:48 -0300

    terminado pt13

---
+++ js/13-ejercicio.js
@@ -0,0 +1,17 @@
+/*
+13- Realiza un script que pida un texto y lo muestre en mayúsculas
+*/
+
+
+
+while(true){
+
+
+    const texto = prompt("ingrese un texto ").toUpperCase();
+
+
+    alert(texto);
+    
+
+}
+
