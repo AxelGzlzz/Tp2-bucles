9cf9154dc219661d72defe1bb75cedc059a0716c
axel gonzalez <ag180066@gmail.com>
2024-07-18 09:17:32 -0300

    se termino pt17

--- js/17-ejercicio.js
+++ js/17-ejercicio.js
@@ -5,3 +5,27 @@
 // Output: la vocal ‘o’ está en la posición
 
 
+  
+
+const texto = prompt('Introduce un texto:');
+
+
+const vocales = 'aeiouAEIOU';
+
+let posicion = -1;
+
+
+
+for (let i = 0; i < texto.length; i++) {
+  if (vocales.indexOf(texto[i]) !== -1) {
+    posicion = i;
+    break;
+  }
+}
+
+
+if (posicion !== -1) {
+  alert("La vocal '" + texto[posicion] + "' está en la posición " + (posicion + 1));
+} else {
+  alert("No se encontraron vocales en el texto.");
+}
